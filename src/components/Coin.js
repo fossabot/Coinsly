9df1e915421ee692e60d86e855af06219d90d81d
Damian Mullins <damian.mullins@just-eat.com>
2018-08-10 16:30:18 +0100

    Warning fix.

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { TickImage, CoinImg, CoinLabel, CoinListItem } from '../styles';
