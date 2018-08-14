28a1ccb298426fdc3f06db9856d3049f465d3492
Damian Mullins <damian.mullins@just-eat.com>
2018-08-14 09:56:03 +0100

    Adding LogRocket.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import LogRocket from 'logrocket';
 
 import App from './components/App';
+
+LogRocket.init('u8t5r0/coinsly');
+Raven.setDataCallback(data => {
+  data.extra.sessionURL = LogRocket.sessionURL;
+  return data;
+});
 
 render(
   <Router>
