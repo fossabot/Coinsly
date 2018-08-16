dac09b3fa614d522d92ee4676b503024687b959b
Damian Mullins <damian.mullins@just-eat.com>
2018-08-16 15:24:07 +0100

    Fixed CoinsList styling.

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -6,22 +6,23 @@ import CoinContainer from '../containers/CoinContainer';
 
 const CoinList = ({ loading, menuOpen, coins, userAuthenticated }) =>
   userAuthenticated ? (
-    coins.length > 0 ? (
-      <fieldset
-        className={`${styles.coinsListWrapper} ${
-          menuOpen ? styles.coinsListWrapper__spacingLeft : ''
-        }`}
-        disabled={loading}
-      >
-        <ul className={styles.coinList}>
-          {coins.map(coin => (
-            <CoinContainer key={coin.id} coin={coin} />
-          ))}
-        </ul>
-      </fieldset>
-    ) : (
-      <p>No coins found</p>
-    )
+    <div
+      className={`${styles.coinsListWrapper} ${
+        menuOpen ? styles.coinsListWrapper__spacingLeft : ''
+      }`}
+    >
+      {coins.length > 0 ? (
+        <fieldset className={styles.coinsList_fieldset} disabled={loading}>
+          <ul className={styles.coinList}>
+            {coins.map(coin => (
+              <CoinContainer key={coin.id} coin={coin} />
+            ))}
+          </ul>
+        </fieldset>
+      ) : (
+        <p>No coins found</p>
+      )}
+    </div>
   ) : null;
 
 CoinList.propTypes = {
